e0db2de84473ea1553dc1cf082685f8fde7c318b
Abdurakhmon Jamoliddinov <milano95a@gmail.com>
2019-05-15 16:19:02 +0500

    Add project files.

---
+++ FileUpload/FileChooserWebChromeClient.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+
+namespace FileUpload
+{
+    public class FileChooserWebChromeClient : WebChromeClient
+    {
+        private static int filechooser = 1;
+        private IValueCallback message;
+        private MainActivity activity = null;
+
+        Action<IValueCallback, Java.Lang.String, Java.Lang.String> callback;
+
+        public FileChooserWebChromeClient(Action<IValueCallback, Java.Lang.String, Java.Lang.String> callback, MainActivity context)
+        {
+            this.activity = context;
+            this.callback = callback;
+        }
+
+        //For Android 4.1
+        [Java.Interop.Export]
+        public void openFileChooser(IValueCallback uploadMsg, Java.Lang.String acceptType, Java.Lang.String capture)
+        {
+            callback(uploadMsg, acceptType, capture);
+        }
+
+        public override bool OnShowFileChooser(WebView webView, IValueCallback filePathCallback, FileChooserParams fileChooserParams)
+        {
+            this.message = filePathCallback;
+            Intent chooserIntent = fileChooserParams.CreateIntent();
+            chooserIntent.AddCategory(Intent.CategoryOpenable);
+            this.activity.StartActivity(Intent.CreateChooser(chooserIntent, "File Chooser"), filechooser, this.OnActivityResult);
+            return true;
+        }
+
+        private void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            if (data != null)
+            {
+                if (requestCode == filechooser)
+                {
+                    if (null == this.message)
+                    {
+                        Console.WriteLine("message is null");
+                        return;
+                    }
+
+                    this.message.OnReceiveValue(WebChromeClient.FileChooserParams.ParseResult((int)resultCode, data));
+                    this.message = null;
+                }
+            }
+        }
+
+    }
+}
---
+++ FileUpload/MainActivity.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Android;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+
+namespace FileUpload
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private static readonly int FILE_CHOOSER_RESULT_CODE = 1;
+        private static readonly int REQUEST_WRITE_EXTERNAL_STORAGE = 2;
+
+        private IValueCallback _mUploadMessage;
+        private Action<int, Result, Intent> _resultCallbackValue;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            ActivityCompat.RequestPermissions(this,
+                new String[] { Manifest.Permission.ReadExternalStorage, Manifest.Permission.WriteExternalStorage },
+                REQUEST_WRITE_EXTERNAL_STORAGE);
+
+            var chrome = new FileChooserWebChromeClient((uploadMsg, acceptType, capture) =>
+            {
+                _mUploadMessage = uploadMsg;
+                var i = new Intent(Intent.ActionGetContent);
+                i.AddCategory(Intent.CategoryOpenable);
+                i.SetType("image/*");
+                StartActivityForResult(Intent.CreateChooser(i, "File Chooser"), FILE_CHOOSER_RESULT_CODE);
+            }, this);
+
+            var wv = this.FindViewById<WebView>(Resource.Id.wv);
+            wv.SetWebViewClient(new WebViewClient());
+            wv.SetWebChromeClient(chrome);
+            wv.Settings.JavaScriptEnabled = true;
+            wv.LoadUrl("http://963c5cbc.ngrok.io/upload.html");
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
+        {
+            if (this._resultCallbackValue != null)
+            {
+                this._resultCallbackValue(requestCode, resultCode, intent);
+                this._resultCallbackValue = null;
+            }
+
+            if (requestCode == FILE_CHOOSER_RESULT_CODE)
+            {
+                if (null == _mUploadMessage)
+                    return;
+                Java.Lang.Object result = intent == null || resultCode != Result.Ok
+                    ? null
+                    : intent.Data;
+                _mUploadMessage.OnReceiveValue(result);
+                _mUploadMessage = null;
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions,
+            Permission[] grantResults)
+        {
+            if (requestCode == REQUEST_WRITE_EXTERNAL_STORAGE)
+            {
+                if ((grantResults.Length > 0) && (grantResults[0] == Permission.Granted))
+                {
+                    Console.WriteLine("permission granted");
+                }
+                else
+                {
+                    Console.WriteLine("permission denied");
+                }
+            }
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void StartActivity(Intent intent, int requestCode, Action<int, Result, Intent> resultCallback)
+        {
+            this._resultCallbackValue = resultCallback;
+            StartActivityForResult(intent, requestCode);
+        }
+
+    }
+}
